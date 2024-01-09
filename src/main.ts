7ec1704f1b7989e68160d38dd8b94d30982ffd17
gravsdev <gravs.dev@gmail.com>
2024-01-09 09:08:09 -0500

    SI-1 Configuración de las variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
-  console.log(configService.get('SERVER_PORT'));
+  app.enableCors({
+    origin: configService.get('ALLOWED_ORIGINS'),
+    credentials: true,
+  });
 
   await app.listen(configService.get('SERVER_PORT'));
 }
